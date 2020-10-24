e17b81cce61c1ad2122f555277e8a87b1752ab8d
Dylan Kario <dylan.kario@gmail.com>
2020-10-23 23:06:04 -0700

    Make the last updated date change at 8pm, not 10pm

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-// Set last updated date to the day of the "most recent 10pm"
-// Ex: if it's now Thurs 8/20 at 10:11pm, it'll be Thurs 8/20
-// Ex: if it's now Thurs 8/20 at 9:11pm, it'll be Wed 8/19
+// Set last updated date to the day of the "most recent 8pm"
+// Ex: if it's now Thurs 8/20 at 8:11pm, it'll be Thurs 8/20
+// Ex: if it's now Thurs 8/20 at 7:11pm, it'll be Wed 8/19
 const lastUpdatedDate = new Date();
 const shortFormat = { month: 'long', day: 'numeric' };
 const longFormat = { ...shortFormat, year: 'numeric' };
 
 lastUpdatedDate.setDate(lastUpdatedDate.getDate() - 1);
-lastUpdatedDate.setHours(lastUpdatedDate.getHours() + 2);
+lastUpdatedDate.setHours(lastUpdatedDate.getHours() + 4);
 
 // If it was gonna be Saturday, make it Friday
 if (lastUpdatedDate.getDay() === 6) {
